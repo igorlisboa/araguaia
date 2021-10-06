19dfdc3069236d7c3c2d261d093e20812f83c306
Igor Lisboa <igorlisboa34@gmail.com>
2021-10-05 23:07:18 -0300

    first commit

---
+++ src/main/java/br/com/araguaia/araguaiasystemjava/AraguaiaSystemJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.araguaia.araguaiasystemjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AraguaiaSystemJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AraguaiaSystemJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/araguaia/araguaiasystemjava/AraguaiaSystemJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.araguaia.araguaiasystemjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AraguaiaSystemJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
